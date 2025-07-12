4c4b788575122042e64aa283760b1e492220fb1c
jnationj <joenationj@gmail.com>
2025-07-12 18:33:42 +0000

    land-agent

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,12 +1,11 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
-import { weatherAgent } from "./agents/weather-agent/weather-agent"; // This can be deleted later
-import { weatherWorkflow } from "./agents/weather-agent/weather-workflow"; // This can be deleted later
-import { yourAgent } from "./agents/your-agent/your-agent"; // Build your agent here
+import { landAgent } from "./agents/land-agent/land-agent"; // Build your agent here
+import { landWorkflow } from "./agents/land-agent/land-workflow";
 
 export const mastra = new Mastra({
-	workflows: { weatherWorkflow }, // can be deleted later
-	agents: { weatherAgent, yourAgent },
+	workflows: { landWorkflow }, // can be deleted later
+	agents: { landAgent },
 	logger: new PinoLogger({
 		name: "Mastra",
 		level: "info",
